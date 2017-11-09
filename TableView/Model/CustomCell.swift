c7ce02991f55b8f25769c8a05d78afbad1ac7891
apple <apple@apples-Mac-mini.local>
2017-11-09 15:57:10 +0530

    New Update

---
+++ TableView/Model/CustomCell.swift
@@ -0,0 +1,18 @@
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet weak var lblTitle: UILabel?
+    @IBOutlet weak var lblDetail: UILabel?
+    @IBOutlet weak var lblConstrain: NSLayoutConstraint?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
